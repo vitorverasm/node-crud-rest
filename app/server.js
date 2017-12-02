bf37c3ab6eb20aab21f284d8b42d34d98509927d
Vitor Veras <vitorverasm@gmail.com>
2017-12-02 16:45:31 -0300

    test route

--- app/server.js
+++ app/server.js
@@ -3,3 +3,30 @@
   Author: Vitor Veras
   Creation date:  02/12/2017
 */
+
+//call packages
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+//setup app variable for bodyparser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//setup listening
+var port = process.env.port || 3000;
+
+//setup routes instance
+var router = express.Router();
+
+//test route
+router.get('/',function (req,res) {
+    res.json({ message: "Welcome to XYZ store"})
+});
+
+//setup /api as root route for our rest api
+app.use('/api',router);
+
+//running
+app.listen(port);
+console.log("Running server on port "+port);
